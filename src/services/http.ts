b701e593a9c619c865bd589b5a5a2ecee1793603
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-31 19:35:23 +0530

    Initial commit

---
+++ src/services/http.ts
@@ -0,0 +1,7 @@
+export async function get(url: string) {
+  const res = await fetch(url, {});
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const data = (await res.json()) as unknown;
+  return data;
+}
